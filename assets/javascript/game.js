eb95a91fab087e1d4d75e3ea9323ab8c6811a961
Meredith Barnett <meredithbarnettcontact@gmail.com>
2020-01-07 16:57:37 -0700

    fixed jewel numbers

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,64 +1,115 @@
-// <!-- Here we have div called "randomNumber" where our random number will go -->
 
 
-
-var random = "";
 
 $(document).ready(function () {
 
-     // ... we generate a random number
-     var random = Math.floor(Math.random() * 120) + 19;
-     console.log(random)
-
-
-     // ... and then dump the random number into our random-number div.
-     $('.rando').text(random);
-})
-     
-
-var wins = 0;
-var losses = 0;
-
-
-
-
-// wins and losses
-
-    if(numGuesses() === random()){
-      wins++;
-      alert("You won");
-      //update the HTML 
-      $('.winBox').text(wins)
-
-    
-  
-  else if (numGuesses > random ){
-      losses++;
-      alert("You lost!");
-
-      $("lossBox").text(losses)
-  }
-
-
-  // assign each jewel a random number between 0-12
-var jewelNumb = "";
-
-$(document).ready(function () {
+  var random = "";
 
   // ... we generate a random number
-  var jewelNumb = Math.floor(Math.random() * 12) + 0;
-  $('.gemImage').text(jewelNumb)
-}
+  var random = Math.floor(Math.random() * 120) + 19;
+  console.log(random)
+  // ... and then dump the random number into our rando div.
+  $('.rando').text(random);
 
-// assign each jewel a jewelNumb
-
-
-
-
+});
 
  
 
-    
+
+  $(document).ready(function () {
+
+  var jewelOne = "";
+  var jewelTwo = "";
+  var jewelThree = "";
+  var jewelFour = "";
+
+  // ... we generate a random number for the 4 crystals and send it to the jewel image
+  var jewelOne = Math.floor(Math.random() * 12 + 1);
+  $('.one').hide(jewelOne);
+
+  var jewelTwo = Math.floor(Math.random() * 12 + 1);
+  $('.two').hide(jewelTwo);
+
+  var jewelThree = Math.floor(Math.random() * 12 + 1);
+  $('.three').hide(jewelThree);
+
+  var jewelFour = Math.floor(Math.random() * 12 + 1);
+  $('.four').hide(jewelFour);
+
+
+  // When you click a jewel, it sends the number to the guesses box
+   // numGuesses = the total of what the users numbers came to, STILL NEED FUNCTION !!!!!!
+
+
+  $("#one").click(function () {
+    $('.guessBox').text(jewelOne);
+    console.log(jewelOne)
+  });
+
+
+  $("#two").click(function () {
+    $('.guessBox').text(jewelTwo);
+    console.log(jewelTwo)
+  });
+
+  $("#three").click(function () {
+    $('.guessBox').text(jewelThree);
+    console.log(jewelThree)
+  });
+
+  $("#four").click(function () {
+    $('.guessBox').text(jewelFour);
+    console.log(jewelFour)
+  });
+
+  
+});
+
+
+  var wins = 0;
+  var losses = 0;
+  var numGuesses = 0;
+
+ 
+  $(document).ready(function () {
+  // wins and losses
+
+  if (numGuesses() === random()) {
+    wins++;
+    alert("You won");
+    //update the HTML 
+    $('.winBox').text(wins);
+
+  }
+
+
+
+  else if (numGuesses > random) {
+    losses++;
+    alert("You lost!");
+
+    $("lossBox").text(losses);
+
+  }
+
+
+
+});
+
+$(document).ready(function () {
+// reset game function
+$("#reset").click(function () {
+  $(resetGAME());
+
+  var resetGAME = ready();
+  
+});
+
+});
+
+
+
+
 
 //  create function that generates a random number and drops it into the randomNumber/rando box DONE
 
@@ -68,8 +119,8 @@ $(document).ready(function () {
 
 // create a function that adds up each random integer into the guessbox
 
-// if guess total goes over the random number, they lose, add one to losses
+// if guess total goes over the random number, they lose, add one to losses  DONE
 
-// the player wins if they get the same number as the randonumb, add one to wins
+// the player wins if they get the same number as the randonumb, add one to wins DONE
 
 // create reset button
